be06444dcf777921f96ec54842594d47fefbdd6a
Shayne Sebro <shayne@shaynes-mbp.attlocal.net>
2020-06-16 21:29:54 -0400

    initial homepage component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Homepage from './components/Homepage/Homepage'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
