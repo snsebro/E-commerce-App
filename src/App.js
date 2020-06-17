437bca3402941078e4a590d26fe8043a41f0f6f5
Shayne Sebro <shayne@shaynes-mbp.attlocal.net>
2020-06-17 12:42:39 -0400

    update menu items to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import Homepage from './pages/Hompage/Homepage'
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Homepage from "./pages/Hompage/Homepage";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route path="/" component={Homepage} />
+      </Switch>
     </div>
   );
 }
--- src/components/Menu-Item/MenuItem.jsx
+++ src/components/Menu-Item/MenuItem.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import {withRouter} from 'react-router-dom'
 import './MenuItem.scss'
 
-export default function MenuItem({title, imageUrl, size}) {
+function MenuItem({title, imageUrl, size, history, linkUrl, match}) {
   return (
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
       <div
         className="background-image"
         style={{
@@ -18,3 +19,5 @@ export default function MenuItem({title, imageUrl, size}) {
         
   )
 }
+
+export default withRouter(MenuItem)
