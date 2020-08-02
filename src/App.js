41c102adec69bb41a6163ba2c8ea9c72432c7fdc
Shayne Sebro <shayne@shaynes-mbp.attlocal.net>
2020-08-02 13:41:35 -0400

    firebase store wuth users into databse

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Homepage from "./pages/Hompage/Homepage";
 import Shop from "./pages/Shop/Shop";
 import "./App.css";
 import Header from "./components/Header/Header";
 import LoginPage from "./pages/LoginPage/LoginPage";
-import {auth} from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      currentUser: null
-    }
+      currentUser: null,
+    };
   }
 
-  unsubscribeFromAuth = null
-  
+  unsubscribeFromAuth = null;
+
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
-      
-      console.log(user)
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              },
+            },
+            () => {
+              console.log(this.state);
+            }
+          );
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
+    });
   }
 
   componentWillUnmount() {
-    this.unsubsribeFromAuth()
+    this.unsubsribeFromAuth();
   }
 
   render() {
@@ -35,12 +51,11 @@ class App extends Component {
         <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={Homepage} />
-          <Route path='/shop' component={Shop} />
-          <Route path='/signin' component={LoginPage} />
+          <Route path="/shop" component={Shop} />
+          <Route path="/signin" component={LoginPage} />
         </Switch>
       </div>
     );
-    
   }
 }
 
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import SignIn from '../../components/SignIn/SignIn'
+import SignUp from '../../components/SignUp/SignUp'
+import './LoginPage.scss'
 
 export default function LoginPage() {
   return (
-    <div>
-       <SignIn />
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+      <SignUp />
     </div>
   )
 }
